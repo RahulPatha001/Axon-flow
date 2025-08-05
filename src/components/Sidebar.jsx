45c6d2783347c85b2ff84ab2c9ca1efc6270c222
RahulPatha001 <rahul.patha29@gmail.com>
2025-08-05 09:25:00 +0530

    ci: implemented side bar and navigation to components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,18 @@
 import { useClerk, useUser } from '@clerk/clerk-react'
+import { Eraser, FileText, Hash, House, Image, Scissors, SquarePen, Users } from 'lucide-react';
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const navItems = [
+  {to:'/ai', label:'Dashboard', Icon: House},
+  {to:'/ai/write-article', label:'Write Article', Icon: SquarePen},
+  {to:'/ai/blog-titles', label:'Blog Titles', Icon: Hash},
+  {to:'/ai/generate-images', label:'Generate Images', Icon:Image },
+  {to:'/ai/remove-background', label:'Remove Background', Icon: Eraser},
+  {to:'/ai/remove-object', label:'Remove Object', Icon: Scissors},
+  {to:'/ai/review-resume', label:'Review Resume', Icon: FileText},
+  {to:'/ai/community', label:'Community', Icon: Users},
+]
 
 function Sidebar({sidebar, setSidebar}) {
     const {user} = useUser();
@@ -11,6 +24,22 @@ function Sidebar({sidebar, setSidebar}) {
         <div className='my-7 w-full'>
             <img src={user.imageUrl} alt="avatar" className='w-10 rounded-full mx-auto' />
             <h1 className='mt-1 text-center'>{user.fullName}</h1>
+            <div>
+              {
+                navItems.map((entry, Index)=>(
+                  <NavLink key={entry.to} to={entry.to} end={entry.to==='/ai'} onClick={()=>setSidebar(false)}
+                  className={({isActive})=>`px-3.5 py-2.5 flex items-center gap-3 rounded 
+                  ${isActive ? 'bg-gradient-to-r from-[#3C81F6] to-[#9234EA] text-white' : ''}`}>
+                    {({isActive}) => (
+                      <>
+                        <entry.Icon className={`w-4 h-4 ${isActive ? 'text-white': ''}`}/>
+                        {entry.label}
+                      </>
+                    )}
+                  </NavLink>
+                ))
+              }
+            </div>
         </div>
     </div>
   )
