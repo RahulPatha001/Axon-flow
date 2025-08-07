91285e3ca52339d56374f0add041e8f160fe454a
RahulPatha001 <rahul.patha29@gmail.com>
2025-08-07 09:07:50 +0530

    ci: completed write article page

--- src/pages/WriteArticle.jsx
+++ src/pages/WriteArticle.jsx
@@ -17,7 +17,7 @@ function WriteArticle() {
     e.preventDefault() // to prevent webpage from reloading
   }
   return (
-    <div className='h-full overflow-y-scroll p-6 items-start flex-wrap gap-4 text-slate-700'>
+    <div className='h-full overflow-y-scroll p-6 items-start flex flex-wrap gap-4 text-slate-700'>
       {/* left */}
       <form className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'
       onSubmit={onSubmitHandler}>
@@ -47,8 +47,19 @@ function WriteArticle() {
         </button>
       </form>
       {/* right */}
-      <div>
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200
+      min-h-96 max-h-[600px]'>
+        <div className='flex items-center gap-3'>
+          <Edit className='w-5 h-5 text-[#4A7AFF]'/>
+          <h1 className='text-xl font-semibold'>Generated Article</h1>
+        </div>
+        <div className='flex-1 flex justify-center items-center'>
+          <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+          <Edit className='w-9 h-9'/>
+          <p>Enter a topic and click "Generate article" to get started</p>
+          </div>
 
+        </div>
       </div>
 
     </div>
