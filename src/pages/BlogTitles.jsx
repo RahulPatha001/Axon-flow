f5e39888532032a506b6d2c2b222ad63e1a8bc72
RahulPatha001 <rahul.patha29@gmail.com>
2025-08-07 22:15:33 +0530

    ci: minor updates to blog titles

--- src/pages/BlogTitles.jsx
+++ src/pages/BlogTitles.jsx
@@ -1,8 +1,68 @@
 import React from 'react'
+import {Edit, Hash, Sparkles} from 'lucide-react'
 
 function BlogTitles() {
+
+
+
+    const blogCategories = ['General', 'Technology','Business','Health','Lifestyle','Education','Travel',
+      'Food'
+    ]
+  
+    const [selectedCategory, setSelectedCategory] = useState('General');
+    const [input, setinput] = useState("")
+  
+    const onSubmitHandler = (e) => {
+      e.preventDefault() // to prevent webpage from reloading
+    }
+
   return (
-    <div>BlogTitles</div>
+    <div className='h-full overflow-y-scroll p-6 items-start flex flex-wrap gap-4 text-slate-700'>
+      {/* left */}
+      <form className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'
+      onSubmit={onSubmitHandler}>
+        <div className='flex items-center gap-3'>
+          <Sparkles className='w-6 text-[#8E37EB]'/>
+          <h1 className='text-xl font-semibold'>AI Title Generator</h1>
+        </div>
+        <p  className='mt-6 text-sm font-medium'>Keyword</p>
+        <input type="text" className='w-full p-3 mt-2 outline-none text-sm rounded-md border border-gray-300'
+        placeholder='Future of AI is....' required
+        onChange={(e)=>setinput(e.target.value)} value={input}/>
+        <p className='mt-4 text-sm font-medium'>Category</p>
+        <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+          {
+            blogCategories.map((item)=>(
+              <span className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedCategory === item ? 'bg-purple-50 text-purple-700' : 
+              'text-gray-500 border-gray-300'}`}
+               key={item} onClick={()=>setSelectedCategory(item)}>{item}</span>
+            ))
+          }
+        </div>
+        <br />
+        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF]
+        to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          <Hash className='w-5'/>
+          Generate Title
+        </button>
+      </form>
+      {/* right */}
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200
+      min-h-96 max-h-[600px]'>
+        <div className='flex items-center gap-3'>
+          <Edit className='w-5 h-5 text-[#4A7AFF]'/>
+          <h1 className='text-xl font-semibold'>Generated Article</h1>
+        </div>
+        <div className='flex-1 flex justify-center items-center'>
+          <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+          <Edit className='w-9 h-9'/>
+          <p>Enter a topic and click "Generate article" to get started</p>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
   )
 }
 
