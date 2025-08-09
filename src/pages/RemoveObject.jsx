5a49ba27b034a5c9cff7357a00716bf66007d291
RahulPatha001 <rahul.patha29@gmail.com>
2025-08-09 20:08:42 +0530

    ci: added all pages and respective data

--- src/pages/RemoveObject.jsx
+++ src/pages/RemoveObject.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import { Scissors, Sparkles } from 'lucide-react'
+import React, { useState } from 'react'
 
 function RemoveObject() {
+    const [input, setinput] = useState("")
+    const [object, setObject] = useState("")
+  
+    const onSubmitHandler = (e) => {
+      e.preventDefault() // to prevent webpage from reloading
+    }
+
   return (
-    <div>RemoveObject</div>
+    <div className='h-full overflow-y-scroll p-6 items-start flex flex-wrap gap-4 text-slate-700'>
+      {/* left */}c
+      <form className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'
+      onSubmit={onSubmitHandler}>
+        <div className='flex items-center gap-3'>
+          <Sparkles className='w-6 text-[#4A7AFF]'/>
+          <h1 className='text-xl font-semibold'>Remove Object</h1>
+        </div>
+        <p  className='mt-6 text-sm font-medium'>Upload Image</p>
+        <input type="file" className='w-full p-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600' required
+        onChange={(e)=>setinput(e.target.files[0])} accept='image/*'/>
+        
+        <p  className='mt-6 text-sm font-medium'>Describe object to remove</p>
+        <textarea rows={4} className='w-full p-3 mt-2 outline-none text-sm rounded-md border border-gray-300'
+        placeholder='eg: spoon, ball or any one object name' required
+        onChange={(e)=>setObject(e.target.value)} value={object}/>
+        
+        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6]
+        to-[#4A7AFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          <Scissors className='w-5'/>
+          Remove Object
+        </button>
+      </form>
+      {/* right */}
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200
+      min-h-96'>
+        <div className='flex items-center gap-3'>
+          <Scissors className='w-5 h-5 text-[#4A7AFF]'/>
+          <h1 className='text-xl font-semibold'>Processed Image</h1>
+        </div>
+        <div className='flex-1 flex justify-center items-center'>
+          <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+          <Scissors className='w-9 h-9'/>
+          <p>Upload an image and click "Remove Object"</p>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
   )
 }
 
