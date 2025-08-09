990da21f5b1ea776ef95fb16f9a928383cb1613d
RahulPatha001 <rahul.patha29@gmail.com>
2025-08-09 20:13:23 +0530

    FIX: restructured data and minor code changes

--- Client/src/pages/community.jsx
+++ Client/src/pages/community.jsx
@@ -24,7 +24,8 @@ function Community() {
             <div key={index} className='relative group inline-block pl-3 pt-3 w-full sm:max-w-1/2
             lg:max-w-1/3'>
               <img src={entry.content} alt="" className='w-full h-full object-cover rounded-lg' />
-              <div className='absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between p-3  group-hover:bg-gradient-to-b from-transparent to-black/80 text-white rounded-lg'>
+              <div className='absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between p-3 
+              group-hover:bg-gradient-to-b from-transparent to-black/80 text-white rounded-lg'>
                 <p className='text-sm hiden group-hover:block'>{entry.prompt}</p>
                 <div className='flex gap-1 items-center'>
                   <p>{entry.likes.length}</p>
